01212a49e3862f7eb069b57ff8090f3f628e4843
pDunCanCode <chesiredarling@gmail.com>
2021-01-23 14:00:34 -0800

    login attempt

--- script.js
+++ script.js
@@ -1,113 +1,117 @@
 var days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
 ];
 var months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
 ];
 var date = new Date();
 var todaysDay = document.getElementById("todaysDay");
 var todaysDate = document.getElementById("todaysDate");
 todaysDay.textContent = days[date.getDay()];
-todaysDate.textContent = months[date.getMonth()] + " " + date.getDate() + ", " + date.getFullYear();
+todaysDate.textContent =
+  months[date.getMonth()] + " " + date.getDate() + ", " + date.getFullYear();
 var formEl = document.getElementById("form");
 var todoInputEl = document.getElementById("todoInput");
 var todoListContainer = document.querySelector(".todo__list");
 function displayTodoDOM(todo) {
-    var liEl = document.createElement("li");
-    liEl.classList.add("bounceIn");
-    liEl.innerHTML = "\n  <span class=\"text\">" + todo + "</span>\n  <div class=\"options\">\n    <span id=\"check\"><i class=\"fa fa-check\"></i></span>\n    <span id=\"edit\"><i class=\"fa fa-edit\"></i></span>\n    <span id=\"trash\"><i class=\"fa fa-trash\"></i></span>\n  </div>\n  ";
-    todoListContainer.appendChild(liEl);
+  var liEl = document.createElement("li");
+  liEl.classList.add("bounceIn");
+  liEl.innerHTML =
+    '\n  <span class="text">' +
+    todo +
+    '</span>\n  <div class="options">\n    <span id="check"><i class="fa fa-check"></i></span>\n    <span id="edit"><i class="fa fa-edit"></i></span>\n    <span id="trash"><i class="fa fa-trash"></i></span>\n  </div>\n  ';
+  todoListContainer.appendChild(liEl);
 }
 function itemToDelete(item) {
-    if (item.classList.contains("fa-trash") || item.id === "trash") {
-        var todoLiEl_1 = item.closest("li");
-        todoLiEl_1.classList.remove("bounceIn");
-        todoLiEl_1.classList.add("bounceOutDown");
-        setTimeout(function () {
-            todoLiEl_1.remove();
-        }, 1000);
-        deleteDataFromLocalStorage(item);
-    }
+  if (item.classList.contains("fa-trash") || item.id === "trash") {
+    var todoLiEl_1 = item.closest("li");
+    todoLiEl_1.classList.remove("bounceIn");
+    todoLiEl_1.classList.add("bounceOutDown");
+    setTimeout(function () {
+      todoLiEl_1.remove();
+    }, 1000);
+    deleteDataFromLocalStorage(item);
+  }
 }
 function itemToEdit(item) {
-    if (item.classList.contains("fa-edit") || item.id === "edit") {
-        var todoLiEl = item.closest("li");
-        todoInputEl.value = todoLiEl.textContent.trim();
-        todoLiEl.remove();
-        editItemFromLocalStorage(item);
-    }
+  if (item.classList.contains("fa-edit") || item.id === "edit") {
+    var todoLiEl = item.closest("li");
+    todoInputEl.value = todoLiEl.textContent.trim();
+    todoLiEl.remove();
+    editItemFromLocalStorage(item);
+  }
 }
 function itemDone(item) {
-    if (item.classList.contains("fa-check") || item.id === "check") {
-        var crossItem_1 = item.closest("li");
-        crossItem_1.firstElementChild.classList.add("completed");
-        crossItem_1.classList.add("rotateOutDownLeft");
-        crossItem_1.addEventListener("transitionend", function () {
-            crossItem_1.remove();
-        });
-        deleteDataFromLocalStorage(item);
-    }
+  if (item.classList.contains("fa-check") || item.id === "check") {
+    var crossItem_1 = item.closest("li");
+    crossItem_1.firstElementChild.classList.add("completed");
+    crossItem_1.classList.add("rotateOutDownLeft");
+    crossItem_1.addEventListener("transitionend", function () {
+      crossItem_1.remove();
+    });
+    deleteDataFromLocalStorage(item);
+  }
 }
 // Local Storage Functions
 function storeToLocalStorage(todo) {
-    var todoArr;
-    if (localStorage.getItem("todos") === null) {
-        todoArr = [];
-    }
-    else {
-        todoArr = JSON.parse(localStorage.getItem("todos"));
-    }
-    todoArr.push(todo);
-    localStorage.setItem("todos", JSON.stringify(todoArr));
+  var todoArr;
+  if (localStorage.getItem("todos") === null) {
+    todoArr = [];
+  } else {
+    todoArr = JSON.parse(localStorage.getItem("todos"));
+  }
+  todoArr.push(todo);
+  localStorage.setItem("todos", JSON.stringify(todoArr));
 }
 function displayDataFromLocalStorage() {
-    var todoArr = JSON.parse(localStorage.getItem("todos"));
-    for (var _i = 0, todoArr_1 = todoArr; _i < todoArr_1.length; _i++) {
-        var todo = todoArr_1[_i];
-        displayTodoDOM(todo);
-    }
+  var todoArr = JSON.parse(localStorage.getItem("todos"));
+  for (var _i = 0, todoArr_1 = todoArr; _i < todoArr_1.length; _i++) {
+    var todo = todoArr_1[_i];
+    displayTodoDOM(todo);
+  }
 }
 function deleteDataFromLocalStorage(item) {
-    var todoArr = JSON.parse(localStorage.getItem("todos"));
-    var todoLiEl = item.closest("li");
-    var todoItemLeft = todoArr.filter(function (todo) { return todoLiEl.textContent.trim() !== todo; });
-    localStorage.setItem("todos", JSON.stringify(todoItemLeft));
+  var todoArr = JSON.parse(localStorage.getItem("todos"));
+  var todoLiEl = item.closest("li");
+  var todoItemLeft = todoArr.filter(function (todo) {
+    return todoLiEl.textContent.trim() !== todo;
+  });
+  localStorage.setItem("todos", JSON.stringify(todoItemLeft));
 }
 function editItemFromLocalStorage(item) {
-    deleteDataFromLocalStorage(item);
+  deleteDataFromLocalStorage(item);
 }
 document.addEventListener("DOMContentLoad", displayDataFromLocalStorage);
 todoListContainer.addEventListener("click", function (e) {
-    itemToDelete(e.target);
-    itemToEdit(e.target);
-    itemDone(e.target);
+  itemToDelete(e.target);
+  itemToEdit(e.target);
+  itemDone(e.target);
 });
 formEl.addEventListener("submit", function (e) {
-    e.preventDefault();
-    var inputTodo = todoInputEl.value;
-    if (!inputTodo) {
-        alert("Please enter a Todo Item");
-    }
-    else {
-        displayTodoDOM(inputTodo);
-        storeToLocalStorage(inputTodo);
-    }
-    formEl.reset();
+  e.preventDefault();
+  var inputTodo = todoInputEl.value;
+  if (!inputTodo) {
+    alert("Please enter a Todo Item");
+  } else {
+    displayTodoDOM(inputTodo);
+    storeToLocalStorage(inputTodo);
+  }
+  formEl.reset();
 });
