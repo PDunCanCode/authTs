83dc5c0ab19b51c6d7fadbb1842218d376fabbc3
pDunCanCode <chesiredarling@gmail.com>
2021-02-09 08:33:17 -0800

    file clean up

--- script.ts
+++ script.ts
@@ -29,12 +29,8 @@ window.onload = async () => {
 const updateUI = async () => {
   const isAuthenticated = await auth0.isAuthenticated();
 
-  (document.getElementById(
-    "btn-logout"
-  ) as HTMLInputElement).disabled = !isAuthenticated;
-  (document.getElementById(
-    "btn-login"
-  ) as HTMLInputElement).disabled = isAuthenticated;
+  document.getElementById("btn-logout").disabled = !isAuthenticated;
+  document.getElementById("btn-login").disabled = isAuthenticated;
 
   if (isAuthenticated) {
     document.getElementById("gated-content").classList.remove("hidden");
@@ -76,6 +72,7 @@ const logout = () => {
     returnTo: window.location.origin,
   });
 };
+
 
 const days: string[] = [
   "Sunday",
